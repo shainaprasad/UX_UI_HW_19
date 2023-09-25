d43097e3985575a3e31887f333987f90cb3c5695
Shaina Prasad <43108299+shainaprasad@users.noreply.github.com>
2023-09-24 21:10:30 -0700

    yikesy

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const DownloadResume = ({ className }) => {
+  return (
+    <div className={`download-resume ${className}`}>
+      <div className="text-wrapper">Download Resume</div>
+    </div>
+  );
+};
